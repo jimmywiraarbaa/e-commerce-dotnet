e4d6d28eb9ba9639f3fa3b60c07d5e99d82be8ef
Jimmy Wira Arbaa <jimmywiraarbaa03@gmail.com>
2025-08-24 14:30:48 +0700

    membuat interface repository

---
+++ Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,43 @@
+using System;
+using Core.Entities;
+using Core.Interfaces;
+
+namespace Infrastructure.Data;
+
+public class ProductRepository : IProductRepository
+{
+    public void AddProduct(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteProduct(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product?> GetProductById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<Product>> GetProductsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool ProductExists(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> SaveChangesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateProduct(Product product)
+    {
+        throw new NotImplementedException();
+    }
+}
